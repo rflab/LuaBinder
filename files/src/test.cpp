f9ba6d2261ae7a2e5300f281f3418d4508328d43
Iwahara Hiroaki <rockfieldlab@gmail.com>
2015-06-25 03:39:21 +0900

    テストコード更新

--- files/src/test.cpp
+++ files/src/test.cpp
@@ -7,22 +7,28 @@
 using namespace std;
 
 // テスト用の関数
-void   func()                 { cout << "A" << endl; }
-string func2(int i)           { cout << "B" << endl; return string("C"); }
-int    overload_func(int i)   { cout << "D " << i << endl; return 100; }
-int    overload_func(string s){ cout << "E " << s << endl; return 200; }
+string f1()        { cout << "f1"    << endl; return string("f1_str"); }
+void   f2(int    n){ cout << "f2_1"  << n << endl; }
+void   f2(string s){ cout << "f2_2 " << s << endl; }
 
-// テスト用のクラス
-class Test
+// テスト用の規定クラス
+class Base
 {
 public:
-	Test()                        { cout << "constructor1" << endl; }
-	Test(string str, int n)       { cout << "constructor2 " << str << n << endl; }
-	~Test()                       { cout << "destructor" << endl; }
-	void   func()                 { cout << "AA" << endl; }
-	string func2(int i)           { cout << "BB" << endl; return string("CC"); }
-	int    overload_func(int i)   { cout << "DD " << i << endl; return 100; }
-	int    overload_func(string s){ cout << "EE " << s << endl; return 200; }
+	Base()                   { cout << "constructor" << endl; }
+	virtual ~Base()          { cout << "destructor" << endl; }
+	virtual int f1()         { cout << "f1" << endl; return 100; }
+	void        f2(int    n) { cout << "f2 " << n << endl; }
+	void        f2(string s) { cout << "f2 overload " << s << endl; }
+};
+
+// テスト用の派生クラス
+class Derived : public Base
+{
+public:
+	Derived(int n) :Base() { cout << "derived constructor " << n << endl; }
+	int f1() override      { cout << "f1 override " << endl; return 200; }
+	void f4(Base& base)    { cout << "userdata argument " << endl; base.f1(); }
 };
 
 int main(int argc, char** argv)
@@ -30,43 +36,43 @@ int main(int argc, char** argv)
 	auto lua = make_shared<rf::LuaBinder>();
 
 	// 関数バインド
-	lua->def("func1", func);
-	lua->def("func2", func2);
-	lua->def("func3", (int(*)(int))    overload_func);
-	lua->def("func4", (int(*)(string)) overload_func);
+	lua->def("func1", f1);
+	lua->def("func2", (void(*)(int))    f2);
+	lua->def("func3", (void(*)(string)) f2);
 
 	// クラスバインド
-	lua->def_class<Test>("Test")->
-		def("new", rf::LuaBinder::constructor<Test()>()).
-		def("new2", rf::LuaBinder::constructor<Test(string, int)>()).
-		def("func1", &Test::func).
-		def("func2", &Test::func2).
-		def("func3", (int(Test::*)(int))    &Test::overload_func).
-		def("func4", (int(Test::*)(string)) &Test::overload_func);
+	lua->def_class<Base>("Base")->
+		def("new",   rf::LuaBinder::constructor<Base()>()).
+		def("func1", &Base::f1).
+		def("func2", (void(Base::*)(int))    &Base::f2).
+		def("func3", (void(Base::*)(string)) &Base::f2);
+
+	// 派生クラスバインド
+	lua->def_subclass<Derived>("Derived", "Base")->
+		def("new", rf::LuaBinder::constructor<Derived(int)>()).
+		def("func1", &Derived::f1).
+		def("func4", &Derived::f4);
 
 	// lua実行
 	lua->dostring(
 		"print(\"----<function>------\")\n"
-		"func1()                        \n"   // 関数テスト
-		"print(func2(100))              \n"
-		"print(func3(200))              \n"
-		"print(func3(100))              \n"
-		"print(func4(\"hoge\"))         \n"
+		"print(func1())                 \n"
+		"func2(1000)                    \n"
+		"func3(\"hoge\")                \n"
 		"print(\"------<class>-------\")\n"
-		"c1 = Test:new()                \n"   // クラスインスタンス化
-		"c1:func1()                     \n"   // メンバ関数テスト
-		"print(c1:func2(100))           \n"
-		"print(c1:func3(200))           \n"
-		"print(c1:func4(\"foo\"))       \n"
+		"c1 = Base:new()                \n"
+		"print(c1:func1())              \n"
+		"c1:func2(2000)                 \n"
+		"c1:func3(\"foo\")              \n"
 		"print(\"------<class2>------\")\n"
-		"c2 = Test:new2(\"hoge \", 100) \n"   // 引数ありコンストラクタでインスタンス化
-		"c2:func1()                     \n"
-		"print(c2:func2(100))           \n"
-		"print(c2:func3(200))           \n"
-		"print(c2:func4(\"foo\"))       \n"
+		"c2 = Derived:new(3000)         \n"
+		"print(c2:func1())              \n"
+		"c2:func2(4000)                 \n"
+		"c2:func3(\"bar\")              \n"
+		"c2:func4(c1)                   \n"
 		"print(\"-----<release>------\")\n"
-		"c1 = nil                       \n"   // インスタンス破棄
-		"c2 = nil                       \n"   // インスタンス破棄
+		"c1 = nil                       \n"
+		"c2 = nil                       \n"
 		"collectgarbage(\"collect\")    \n");
 
 	// for windows console
